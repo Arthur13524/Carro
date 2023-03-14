0ff85b3a3a7a32efa489e9a5aca10563cc9f4d18
Arthur13524 <arthurfeliciano271@gmail.com>
2023-03-15 04:32:53 +0900

    aaaaa

--- www/components/car.js
+++ www/components/car.js
@@ -1,12 +1,20 @@
-function calcularEficiencia() {
-	var marca = document.getElementById("marca").value;
-	var modelo = document.getElementById("modelo").value;
-	var quilometragem = parseFloat(document.getElementById("quilometragem").value);
-	var litros = parseFloat(document.getElementById("litros").value);
+function calcularconsumo(){
+ 
+ var km = document.getElementById ('km').value;
+ var gas = document.getElementById('gas').value;
 
-			var eficiencia = quilometragem / litros;
+ var media = km / gas;
 
-			var resultado = "Seu " + marca + " " + modelo + " fez " + eficiencia.toFixed(2) + " quilômetros por litro."
-
-			document.getElementById("resultado").innerHTML = resultado
+if(media >20){
+ document.getElementById("Extremamente").innerHTML = "<h3><=20km/l | Extremamente Econômico</h3>"
+} 
+else if((media >=12) && (media <20)) {
+    document.getElementById("Economico").innerHTML = "<h3> <=12km/l---->20km/l | Econômico</h3>"
+}
+else if((media >=8) && (media <12)) {
+    document.getElementById("Normal").innerHTML = "<h3><=8km/l---->12km/l | Normal</h3>"
+}
+else{
+    document.getElementById("Alto").innerHTML = "<h3>>8km/l | Alto consumo</h3>"
+}
 }
